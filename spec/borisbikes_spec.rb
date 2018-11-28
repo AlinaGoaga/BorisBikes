b3e1e805ca7156249e2126380c0265c8e5d24713
Alina Goaga <goaga.alina@gmail.com>
2018-11-28 14:22:23 +0000

    Created bike class and spec

--- spec/borisbikes_spec.rb
+++ spec/borisbikes_spec.rb
@@ -1,3 +1,11 @@
 require 'borrisbikes'
+
+#describe DockingStation do
+#  it 'responds to release_bike' do
+#    expect(docking_station).to respond_to :release_bike
+#  end
+#end
+
 describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
 end
