d769132da9e7c932d3447d78b98be32c93631b3d
Alina Goaga <goaga.alina@gmail.com>
2018-12-04 10:09:37 +0000

    Refactored to use subject instead of the before each block

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,9 +1,9 @@
 require 'station'
 
 RSpec.describe DockingStation do
-  before(:each) do
-    @station = DockingStation.new
-  end
+  # before(:each) do
+  #   @station = DockingStation.new
+  # end
 
   it { is_expected.to respond_to :bikes } # testing reader
 
@@ -15,40 +15,39 @@ RSpec.describe DockingStation do
     end
 
     it 'defaults capacity' do
-      DockingStation::DEFAULT_CAPACITY.times {@station.dock_bike(Bike.new)}
-      expect { @station.dock_bike(Bike.new) }.to raise_error 'Docking station full'
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(Bike.new) }
+      expect { subject.dock_bike(Bike.new) }.to raise_error 'Station full'
     end
   end
-end
 
-describe '#dock_bike' do
-  it 'returns a station that includes the bike which has just been docked' do
-    bike = Bike.new
-    expect(@station.dock_bike(bike)).to include(bike)
+  describe '#dock_bike' do
+    it 'returns a station that includes the bike which has just been docked' do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to include(bike)
+    end
+
+    it 'raises an error when we try to add another bike if the station has 20 bikes already' do
+      subject.capacity.times { subject.dock_bike(Bike.new) }
+      expect { subject.dock_bike(Bike.new) }.to raise_error 'Station full'
+    end
   end
 
-  it 'raises an error when we try to add another bike if the station has 20 bikes already' do
-    @station.capacity.times { @station.dock_bike(Bike.new) }
-    expect { @station.dock_bike(Bike.new) }.to raise_error 'Station full'
-  end
-end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to eq bike
+    end
 
-describe '#release_bike' do
-  it 'releases a bike' do
-    bike = Bike.new
-    @station.dock_bike(bike) # docking the bike in the station
-    expect(@station.release_bike).to eq bike
-  end
+    it 'always releases a working bike' do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      released_bike = subject.release_bike
+      expect(released_bike.working?).to be true
+    end
 
-  it 'always releases a working bike' do
-    bike = Bike.new
-    @station.dock_bike(bike)
-    released_bike = @station.release_bike
-    expect(released_bike.working?).to be true
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
-
-  it 'raises an error when there are no bikes available' do
-    expect { @station.release_bike }.to raise_error 'No bikes available'
-  end
-end
 end
